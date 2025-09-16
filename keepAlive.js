d9df43b5757638a8ad99d79bb8875f7797877b87
Shina Adeleye <adeleyeshina4@gmail.com>
2025-09-16 09:55:46 -0700

    updated mvp

--- keepAlive.js
+++ keepAlive.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 cron.schedule('*/13 * * * *', async () => {
     try {
-        const response = await axios.get('http://localhost:5000/')
+        const response = await axios.get('https://castle-backend-tbl9.onrender.com')
         console.log(`Health check response: ${response.status}`);
 
     } catch (error) {
